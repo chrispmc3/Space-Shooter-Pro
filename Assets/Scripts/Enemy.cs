9ffba998f65f31e8b113567b837e2d134a877b9f
Chris Dev <chrisdev@christians-MacBook-Pro-2.local>
2024-07-27 08:27:53 -0500

    Work up until enemy laser.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,16 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 4.0f;
+    [SerializeField]
+    private GameObject _laserPrefab;
 
     private Player _player;
     private Animator _anim;
     private BoxCollider2D _enemyCollider;
     private AudioSource _audioSource;
+    private float _fireRate = 3.0f;
+    private float _canFire = -1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +41,24 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       CalculateMovement();
+
+       if (Time.time > _canFire)
+       {
+            _fireRate = Random.Range(3f, 7f);
+            _canFire = Time.time + _fireRate;
+            GameObject enemyLaser = Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+            Laser[] lasers = enemyLaser.GetComponentsInChildren<Laser>();
+            
+            for (int i = 0; i < lasers.Length; i++)
+            {
+                lasers[i].EnemyLaser();
+            }
+       }
+    }
+
+    void CalculateMovement()
+    {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
        if (transform.position.y < -6f)
@@ -44,6 +67,7 @@ public class Enemy : MonoBehaviour
             transform.position = new Vector3(randomX, 8, 0);
         }
     }
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -8,11 +8,26 @@ public class Laser : MonoBehaviour
     [SerializeField]
     private float _speed = 8.0f;
 
+    private bool _isEnemyLaser = false;
+
     // Update is called once per frame
     void Update()
     {
+        if (_isEnemyLaser == false)
+        {
+            MoveUp();
+        }
+        else
+        {
+            MoveDown();
+        }
+       
 
-        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+    }
+
+    void MoveUp()
+    {
+         transform.Translate(Vector3.up * _speed * Time.deltaTime);
 
         if (transform.position.y > 8f)
         {
@@ -23,6 +38,25 @@ public class Laser : MonoBehaviour
 
             Destroy(this.gameObject);
         }
+    }
 
+    void MoveDown()
+    {
+         transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -8f)
+        {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void EnemyLaser()
+    {
+        _isEnemyLaser = true;
     }
 }
